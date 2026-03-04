2f1f61ef02edd8807f6438310f3cdde995793821
allseenn <allseen@live.ru>
2026-03-04 17:17:55 +0000

    proxy.sh second version async

--- proxy.sh
+++ proxy.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 . $(dirname $0)/.env
-LIST=$(curl -sL $PROXY_LIST_RU | sed -e 's/\:/ /g' | sed -e 's/https/http/g' | sed -e 's/\/\///g')
-sudo bash -c "cat << EOF > /etc/proxychains.conf
-[ProxyList]
-$LIST
-EOF"
+START_TIME=$(date +%s)
+LIST_RAW=$(curl -sL $PROXYFLY_LIST | sort | uniq)
+AMOUNT_RAW=$(echo "$LIST_RAW" | wc -l)
+COUNT_PROCESSED=0
+COUNT_TESTED=0
+TIMEOUT=1
+FILE_SAVE=/etc/proxychains.conf
+echo "Loaded $AMOUNT_RAW proxies, estimated time to test $(echo $AMOUNT_RAW*$TIMEOUT | bc) seconds"
+echo "[ProxyList]" > /etc/proxychains.conf
+
+for i in $LIST_RAW
+do
+  HOST_REAL=$(curl -s -m $TIMEOUT -x $i $PROXYFLY_TARGET &)
+  HOST_LIST=$(echo $i | awk -F: '{print $2}' | sed 's/\/\///g')
+  COUNT_PROCESSED=$((COUNT_PROCESSED+1))
+  if [ "$HOST_LIST" == "$HOST_REAL" ]; then
+    echo $i | sed -e 's/\:/ /g' | sed -e 's/https/http/g' | sed -e 's/\/\///g' >> $FILE_SAVE
+    COUNT_TESTED=$((COUNT_TESTED+1))
+    echo "$COUNT_TESTED $i remains $((AMOUNT_RAW-COUNT_PROCESSED))"
+  fi
+done
+wait
+END_TIME=$(date +%s)
+echo "Done in $(echo "($END_TIME-$START_TIME)/60" | bc -l) minutes"
+echo "Tested $COUNT_PROCESSED hosts, found $COUNT_TESTED working hosts"
