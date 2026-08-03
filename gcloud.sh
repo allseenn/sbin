acd192f526011537c96b5009eede532d5f00eb8f
allseenn <allseen@live.ru>
2026-08-04 00:05:05 +0300

    ~gcloud add 0.0.0.0 for socks

--- gcloud.sh
+++ gcloud.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ## Socks tunnel to Google Cloud Shell
-gcloud cloud-shell ssh --ssh-flag="-D 8080" --ssh-flag="-C"
+gcloud cloud-shell ssh --ssh-flag="-D 0.0.0.0:8080" --ssh-flag="-C"
