5da6fddde52b4a094a9571bcb65edd5091f32eb0
allseenn <allseen@live.ru>
2026-09-26 17:06:51 +0000

    fix: "-"

--- jl.sh
+++ jl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # script controls jupiter $TYPE server installed to virtual environment in directory VENV 
-VENV="${PREFIX:/}/opt/lab/.venv"
+VENV="${PREFIX:-}/opt/lab/.venv"
 # NDIR - its working dir place where you $TYPEs *.ipynb files stores.
 NDIR=/mnt/sdcard/github
 # 0.0.0.0 start to listen any available net interface
