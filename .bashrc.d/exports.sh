5d2adab06a60d0a2cd9e9545d9868598526e61c5
allseenn <allseen@live.ru>
2026-09-26 11:09:29 +0000

    feat: add bin-path for termux ~exports.sh

--- .bashrc.d/exports.sh
+++ .bashrc.d/exports.sh
@@ -1,6 +1,5 @@
 export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-export PATH=$PATH:/usr/sbin/:.local/bin
+export PATH=$PATH:/usr/sbin/:.local/bin:/data/data/com.termux/files/usr/sbin:/data/data/com.termux/files/home/.local/bin
 export LESSCHARSET=utf-8
 export PATH=$PATH:/opt/ardupilot/Tools/autotest
 export PATH=/usr/lib/ccache:$PATH
-export PATH=$PATH:/home/Rostislav/.local/share/gem/ruby/3.3.0/bin
