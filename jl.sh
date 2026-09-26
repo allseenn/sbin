e62654f6f2dcf8cf6ccb088fe37c2f8eb624934c
allseenn <allseen@live.ru>
2026-09-26 17:24:29 +0000

    fix: uv venv --python $(which python) --system-site-packages "$VENV"

--- jl.sh
+++ jl.sh
@@ -17,7 +17,8 @@ CMD=${1:-h}
 make_venv() {
     mkdir -p "$(dirname "$VENV")"
     # uv python install 3.12
-    uv venv --python 3.14 --system-site-packages "$VENV"
+    # uv venv --python 3.14 --system-site-packages "$VENV"
+    uv venv --python $(which python) --system-site-packages "$VENV"
     uv pip install --python "$VENV/bin/python" jupyterlab numpy pandas matplotlib scipy codeium-jupyter
 }
 
