3713127403785c69c383d2cc20ae1c73448642da
allseenn <allseen@live.ru>
2026-09-26 17:32:18 +0000

    fix: --break-system-packages

--- jl.sh
+++ jl.sh
@@ -19,7 +19,7 @@ make_venv() {
     # uv python install 3.12
     uv venv --python 3.14 --system-site-packages "$VENV"
     # uv venv --python $(which python) --system-site-packages "$VENV"
-    uv pip install --no-build psutil --python "$VENV/bin/python" jupyterlab numpy pandas matplotlib scipy codeium-jupyter
+    uv pip install --break-system-packages --no-build psutil --python "$VENV/bin/python" jupyterlab numpy pandas matplotlib scipy codeium-jupyter
 }
 
 ensure_venv() {
